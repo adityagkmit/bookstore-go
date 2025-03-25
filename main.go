dea29a51dee4d7c65d54edd666ca7488affc247d
Aditya Singh Rajawat <adityarajawat@gkmit.co>
2025-03-25 13:46:44 +0530

    feat: add mongodb connection

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	config "github.com/adityagkmit/bookstore/utils"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
@@ -15,14 +16,23 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
-
 	// Initialize router
 	r := chi.NewRouter()
+
+	// Connect to Database
+	db, err := config.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Database connection established: ", db.Name())
 	// Start server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
 	fmt.Println("Server running on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
+
 }
