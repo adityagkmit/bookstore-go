273aeb33d1b69a550a6447acf7ed536778ea1d97
Aditya Singh Rajawat <adityarajawat@gkmit.co>
2025-03-25 11:38:11 +0530

    feat: add server setup

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	// Initialize router
+	r := chi.NewRouter()
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
