495a8a741797e797387336f0490a5e39bc6bddb1
Aditya Singh Rajawat <adityarajawat@gkmit.co>
2025-03-26 14:57:11 +0530

    chore: add new common utils file for common functions

---
+++ utils/common.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashes a given password
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed), err
+}
+
+// VerifyPassword checks if the provided password is correct
+func VerifyPassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
+
+// GenerateJWT generates a JWT token
+func GenerateJWT(email string) (string, error) {
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey == "" {
+		return "", errors.New("JWT_SECRET is not set")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": email,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	return token.SignedString([]byte(secretKey))
+}
